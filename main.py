3d96211ef7a7bc8cfc9af9e920312f04e9366756
kwa_laa <khoaddnguyen@gmail.com>
2023-11-20 15:52:50 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not t: return True
+        if not s: return False
+
+        if self.isSameTree(s, t):
+            return True
+
+        return(self.isSubtree(s.left, t) or
+               self.isSubtree(s.right, t))
+
+
+    def isSameTree(self, s, t) -> bool:
+        if not s and not t:  # check if p and q are empty trees
+            return True
+        if not s or not t or s.val != t.val:  # check if one of the tree is null
+            return False
+
+        return (self.isSameTree(s.left, t.left) and
+                    self.isSameTree(s.right, t.right))
